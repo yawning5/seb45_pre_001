e5c8b42d29d132b19a02334791c358f39e69fe91
yawn <yawning055@gmail.com>
2023-08-22 01:33:17 +0900

    QuestionDto에 의문 추가

--- Server/src/main/java/com/stackoverflow/Server/question/dto/QuestionDto.java
+++ Server/src/main/java/com/stackoverflow/Server/question/dto/QuestionDto.java
@@ -18,6 +18,8 @@ public class QuestionDto {
         private String title;
         private String questionBody;
 
+        // 여기서 memberId를 받아오는데 굳이 nickname까지 받아와야 할까?
+        // service 나 controller에서 member객체를 Id로 찾아서 세팅해주면 되는거 같은데?
         public Member getMember() {
             Member member = new Member();
             member.setMemberId(memberId);
